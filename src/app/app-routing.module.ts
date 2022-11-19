cc2bc08dca3786abbeaf4b2d143f6ab3eae6241b
Elisberto Junior <elisberto_jr@hotmail.com>
2022-11-19 17:53:44 -0300

    add routes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,22 @@
+import { CardComponent } from './pages/portifolio/card/card.component';
+import { TitleComponent } from './pages/index/title/title.component';
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
 const routes: Routes = [
-
+  {
+    path: '',
+    component: TitleComponent,
+    pathMatch: 'full'
+  },
+  {
+    path: 'portifolio',
+    component: CardComponent,
+    pathMatch: 'prefix'
+  },
+  {
+    path: '**', redirectTo: ''
+  }
 ];
 
 @NgModule({
